2f61eb3a4d675108e6e32d117f58f36fa85c8f88
WZidane <wail.zidane@hotmail.com>
2025-06-12 17:23:15 +0000

    fix: sql database

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS trips (
     price DECIMAL(6,2) NOT NULL,                                             
     description TEXT,                          
     vehicule VARCHAR(100),                            
-    status TINYINT(1) NOT NULL DEFAULT 0,
+    trip_type TINYINT(1) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
     FOREIGN KEY (driver_id) REFERENCES users(id) ON DELETE CASCADE
@@ -35,10 +35,8 @@ CREATE TABLE IF NOT EXISTS bookings (
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE
-)
+);
 
 INSERT INTO users (username, email, password) VALUES
 ('alice', 'alice@example.com', '$argon2i$v=19$m=65536,t=4,p=1$kl4mG0NIEyx0xJ0XjEnYWQ$jaE4X6av6OhLfYC+tWOyrs+Z/Ab6hqm5TOE6KMS2/9Q'),
 ('bob', 'bob@example.com', '$argon2i$v=19$m=65536,t=4,p=1$kl4mG0NIEyx0xJ0XjEnYWQ$jaE4X6av6OhLfYC+tWOyrs+Z/Ab6hqm5TOE6KMS2/9Q'),
-('princesse caroline', 'pcaroline@example.com', '$argon2i$v=19$m=65536,t=4,p=1$kl4mG0NIEyx0xJ0XprincessecarolinehLfYC+tWOyrs+Z/Ab6hqm5TOE6KMS2/9Q'),
-('patrick fiori', 'pfiori@example.com', '$argon2i$v=19$m=65536,t=4,p=1$kl4mG0NIEyx0xJ0XpatrickfioriYC+tWOyrs+Z/Ab6hqm5TOE6KMS2/9Q');
